5b02440616bcea60dff83fb4bcae5d0efe788986
Pietro <pietro.carnevale.01@gmail.com>
2023-12-14 16:44:57 +0100

    app icon and name

--- G2/GameScene.swift
+++ G2/GameScene.swift
@@ -273,9 +273,9 @@ extension GameScene {
 extension GameScene {
     
     func SetUp() {
-        let node1 = SKSpriteNode(imageNamed: "Heart")
-        let node2 = SKSpriteNode(imageNamed: "Heart")
-        let node3 = SKSpriteNode(imageNamed: "Heart")
+        let node1 = SKSpriteNode(imageNamed: "heart")
+        let node2 = SKSpriteNode(imageNamed: "heart")
+        let node3 = SKSpriteNode(imageNamed: "heart")
         heartsNodes.append(node1)
         heartsNodes.append(node2)
         heartsNodes.append(node3)
